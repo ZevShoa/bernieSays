c5d59096206999575fd2c5fe3f562616335e5728
Zevshoa <zshoag@gmail.com>
2016-02-29 09:50:41 -0500

    Added logic but still some problems

--- bernieSays/playScreen.cs
+++ bernieSays/playScreen.cs
@@ -15,6 +15,7 @@ namespace bernieSays
     {
         int listNum = 0;
         int randNum;
+        int patternNum = 0;
         List<int> pattern = new List<int>();
         Random choose = new Random();
         public playScreen()
@@ -34,47 +35,100 @@ namespace bernieSays
             randNum = choose.Next(0, 4);
             pattern.Add(randNum);
             
+            listNum = pattern.Count();
+            for (int i =0;i <listNum; i++)
+            {
+                if (pattern[i] == 0)
+                {
+                    buttonChoose0.PerformClick();
                 }
+                else if (pattern[i] == 1)
+                {
+                    buttonChoose1.PerformClick();
+                }
+                else if (pattern[i] == 2)
+                {
+                    buttonChoose2.PerformClick();
+                }
+                else if (pattern[i] == 3)
+                {
+                    buttonChoose3.PerformClick();
+                }
+
+            }
+            patternNum++;
+            randNum = 0;
+
+        }
 
         private void buttonChoose0_Click(object sender, EventArgs e)
         {
-            buttonChoose0.BackgroundImage = Properties.Resources.bernie0;
-            buttonChoose0.Text = "";
-            Refresh();
-            Thread.Sleep(1000);
-            buttonChoose0.BackgroundImage = null;
-            buttonChoose0.Text = "Save the Enviroment";
+            if (pattern[patternNum] == 0)
+            {
+                buttonChoose0.BackgroundImage = Properties.Resources.bernie0;
+                buttonChoose0.Text = "";
+                Refresh();
+                Thread.Sleep(1000);
+                buttonChoose0.BackgroundImage = null;
+                buttonChoose0.Text = "Save the Enviroment";
+
+            }
+            else
+            {
+                this.BackgroundImage = null;
+            }
         }
 
         private void buttonChoose1_Click(object sender, EventArgs e)
         {
-            buttonChoose1.BackgroundImage = Properties.Resources.bernie1;
-            buttonChoose1.Text = "";
-            Refresh();
-            Thread.Sleep(1000);
-            buttonChoose1.BackgroundImage = null;
-            buttonChoose1.Text = "Fight Against the Richest One Percent";
+            if (pattern[patternNum] == 1)
+            {
+                buttonChoose1.BackgroundImage = Properties.Resources.bernie1;
+                buttonChoose1.Text = "";
+                Refresh();
+                Thread.Sleep(1000);
+                buttonChoose1.BackgroundImage = null;
+                buttonChoose1.Text = "Fight Against the Richest One Percent";
+            }
+            else
+            {
+                this.BackgroundImage = null;
+            }
 
         }
 
         private void buttonChoose2_Click(object sender, EventArgs e)
         {
-            buttonChoose2.BackgroundImage = Properties.Resources.bernie2;
-            buttonChoose2.Text = "";
-            Refresh();
-            Thread.Sleep(1000);
-            buttonChoose2.BackgroundImage = null;
-            buttonChoose2.Text = "Socialized Medicare";
+            if (pattern[patternNum] == 2)
+            {
+                buttonChoose2.BackgroundImage = Properties.Resources.bernie2;
+                buttonChoose2.Text = "";
+                Refresh();
+                Thread.Sleep(1000);
+                buttonChoose2.BackgroundImage = null;
+                buttonChoose2.Text = "Socialized Medicare";
+            }
+            else
+            {
+                this.BackgroundImage = null;
+            }
         }
 
         private void buttonChoose3_Click(object sender, EventArgs e)
         {
-            buttonChoose3.BackgroundImage = Properties.Resources.bernie3;
-            buttonChoose3.Text = "";
-            Refresh();
-            Thread.Sleep(1000);
-            buttonChoose3.BackgroundImage = null;
-            buttonChoose3.Text = "Social Justice";
+            if (pattern [patternNum] == 3)
+            {
+                buttonChoose3.BackgroundImage = Properties.Resources.bernie3;
+                buttonChoose3.Text = "";
+                Refresh();
+                Thread.Sleep(1000);
+                buttonChoose3.BackgroundImage = null;
+                buttonChoose3.Text = "Social Justice";
+            }
+            else
+            {
+                this.BackgroundImage = null;
+            }
         }
     }
 }
