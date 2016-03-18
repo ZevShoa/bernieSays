4273398be68b80a474b64e3bd8362ec987a1c2a9
Zevshoa <zshoag@gmail.com>
2016-03-18 12:20:58 -0400

    Finished the new program with lists and arrays

--- bernieSays/playScreen.cs
+++ bernieSays/playScreen.cs
@@ -20,9 +20,7 @@ namespace bernieSays
     public partial class playScreen : UserControl
     {
         // created all my variables
-        int listNum = 0;
         int randNum;
-        int patternNum = -1;
         int patternSpot =0 ;
        
         //The only thing I needed public was my list
@@ -39,14 +37,14 @@ namespace bernieSays
         Random choose = new Random();
         public playScreen()
         {
-            buttons[0] = buttonChoose0;
+           
             InitializeComponent();
             
         }
          
         private void playScreen_Load(object sender, EventArgs e)
         {
-            //adds the buttons to the list
+            //adds the buttons to the list  
             buttons[0] = buttonChoose0;
             buttons[1] = buttonChoose1;
             buttons[2] = buttonChoose2;
@@ -63,90 +61,54 @@ namespace bernieSays
         {
             //all this is the computer clicking the buttons
             patternSpot = 0;
-            patternNum++;
             randNum = choose.Next(0, 4);
             pattern.Add(randNum);
             
-            listNum = pattern.Count();
-            for (int i =0;i <listNum; i++)
+            
+            for (int i =0;i < pattern.Count(); i++)
             {
-                if (pattern[i] == 0)
-                {
-                  
-                    buttonChoose0.BackgroundImage = Properties.Resources.bernie0;
-                    buttonChoose0.Text = "";
-                    player.Play();
-                    Refresh();
-                    Thread.Sleep(1000);
-                    buttonChoose0.BackgroundImage = null;
-                    buttonChoose0.Text = "Save the Environment";
-                    Thread.Sleep(500);
-
-                    Refresh();
-                }
-                else if (pattern[i] == 1)
-                {
-                   
-                    buttonChoose1.BackgroundImage = Properties.Resources.bernie1;
-                    buttonChoose1.Text = "";
-                    player.Play();
-                    Refresh();
-                    Thread.Sleep(1000);
-                    buttonChoose1.BackgroundImage = null;
-                    buttonChoose1.Text = "Fight Against the Richest One Percent";
-                    Thread.Sleep(500);
-                    Refresh();
-
-                }
-                else if (pattern[i] == 2)
-                {
-              
-                    buttonChoose2.BackgroundImage = Properties.Resources.bernie2;
-                    buttonChoose2.Text = "";
-                    player.Play();
-                    Refresh();
-                    Thread.Sleep(1000);
-                    buttonChoose2.BackgroundImage = null;
-                    buttonChoose2.Text = "Socialized Medicare";
-                    Thread.Sleep(500);
-                    Refresh();
-                }
-                else if (pattern[i] == 3)
-                {
-               
-                    buttonChoose3.BackgroundImage = Properties.Resources.bernie3;
-                    buttonChoose3.Text = "";
-                    player.Play();
-                    Refresh();
-                    Thread.Sleep(1000);
-                    buttonChoose3.BackgroundImage = null;
-                    buttonChoose3.Text = "Social Justice";
-                    Thread.Sleep(500);
-                    Refresh();
-                }
-
-            }
-            //resets the random number for next time
-            randNum = 0;
-
-        }
-        public void buttonsClicks()
-        {
-
-            for (int i = 0; i < pattern.Count; i++)
-            {
-
                 player.Play();
                 buttons[pattern[i]].BackgroundImage = bernies[pattern[i]];
                 buttons[pattern[i]].Text = "";
                 Refresh();
-                Thread.Sleep(1000);
+                Thread.Sleep(500);
                 buttons[pattern[i]].BackgroundImage = null;
                 buttons[pattern[i]].Text = phrases[pattern[i]];
-                
+                patternSpot++;
+                Thread.Sleep(500);
+                Refresh();
             }
-            patternSpot++;
-
+            
+            //resets the random number for next time
+            randNum = 0;
+            patternSpot = 0;
+        }
+        public void buttonsClicks(int buttonNum)
+        {
+            if (pattern.Count() > patternSpot)
+            {
+                if (pattern[patternSpot] == buttonNum)
+                {
+                    player.Play();
+                    buttons[pattern[patternSpot]].BackgroundImage = bernies[pattern[patternSpot]];
+                    buttons[pattern[patternSpot]].Text = "";
+                    Refresh();
+                    Thread.Sleep(500);
+                    buttons[pattern[patternSpot]].BackgroundImage = null;
+                    buttons[pattern[patternSpot]].Text = phrases[pattern[patternSpot]];
+                    Thread.Sleep(500);
+                    Refresh();
+                }
+                else if (pattern.Count() > patternSpot)
+                {
+                    loose();
+                }
+                patternSpot++;
+            }
+            if (pattern.Count <= patternSpot)
+            {
+                computerTurn();
+            }
         }
         public void loose()
         {
@@ -164,136 +126,28 @@ namespace bernieSays
         {
            
             // when the user chooses one of the buttons this sees if it is the right button
-            if (pattern[patternSpot] == 0)
-            {
-                buttonsClicks();             
-                //player.Play();
-                //buttonChoose0.BackgroundImage = Properties.Resources.bernie0;
-                //buttonChoose0.Text = "";
-                //Refresh();
-                //Thread.Sleep(1000);
-                //buttonChoose0.BackgroundImage = null;
-                //buttonChoose0.Text = "Save the Environment";
-                //patternSpot++;
-                
-                if (patternSpot == pattern.Count)
-                {
-                    computerTurn();
-                }
-            }
-            else
-            {
-                loose();
-                //SoundPlayer player = new SoundPlayer(Properties.Resources.loose);
-                //player.Play();
-                //Form f = this.FindForm();
-                //f.Controls.Remove(this);
-
-                //endScreen es = new endScreen();
-                //f.Controls.Add(es);
-            }
+                buttonsClicks(0);             
         }
 
         private void buttonChoose1_Click(object sender, EventArgs e)
         {
             // when the user chooses one of the buttons this sees if it is the right button
-            if (pattern[patternSpot] == 1)
-            {
-                buttonsClicks();
-                //player.Play();
-                //buttonChoose1.BackgroundImage = Properties.Resources.bernie1;
-                //buttonChoose1.Text = "";
-                //Refresh();
-                //Thread.Sleep(1000);
-                //buttonChoose1.BackgroundImage = null;
-                //buttonChoose1.Text = "Fight Against the Richest One Percent";
-                //patternSpot++;
-           
-                if (patternSpot == pattern.Count)
-                {
-                    computerTurn();
-                }
-            }
-            else
-            {
-                loose();
-                //SoundPlayer player = new SoundPlayer(Properties.Resources.loose);
-                //player.Play();
-
-                //Form f = this.FindForm();
-                //f.Controls.Remove(this);
-
-                //endScreen es = new endScreen();
-                //f.Controls.Add(es);
-            }
+                buttonsClicks(1);
 
         }
 
         private void buttonChoose2_Click(object sender, EventArgs e)
         {
             // when the user chooses one of the buttons this sees if it is the right button
-            if (pattern[patternSpot] == 2)
-            {
-                buttonsClicks();
-                //player.Play();  
-                //buttonChoose2.BackgroundImage = Properties.Resources.bernie2;
-                //buttonChoose2.Text = "";
-                //Refresh();
-                //Thread.Sleep(1000);
-                //buttonChoose2.BackgroundImage = null;
-                //buttonChoose2.Text = "Socialized Medicare";
-                //patternSpot++;
-                
-                if (patternSpot == pattern.Count)
-                {
-                    computerTurn();
-                }
-            }
-            else
-            {
-                loose();
-               // SoundPlayer player = new SoundPlayer(Properties.Resources.loose);
-               // player.Play();
-               // Form f = this.FindForm();
-               // f.Controls.Remove(this);
+                buttonsClicks(2);
 
-               //endScreen es = new endScreen();
-               // f.Controls.Add(es);
-            }
         }
 
         private void buttonChoose3_Click(object sender, EventArgs e)
         {
             
             // when the user chooses one of the buttons this sees if it is the right button
-            if (pattern [patternSpot] == 3)
-            {
-                buttonsClicks();
-                //player.Play();
-                //buttonChoose3.BackgroundImage = Properties.Resources.bernie3;
-                //buttonChoose3.Text = "";
-                //Refresh();
-                //Thread.Sleep(1000);
-                //buttonChoose3.BackgroundImage = null;
-                //buttonChoose3.Text = "Social Justice";
-                //patternSpot++;
-               
-                if (patternSpot == pattern.Count)
-                {
-                    computerTurn();
-                }
-            }
-            else
-            {
-                loose();
-                //SoundPlayer player = new SoundPlayer(Properties.Resources.loose);
-                //player.Play();
-                //Form f = this.FindForm();
-                //f.Controls.Remove(this);
-
-                //endScreen es = new endScreen();
-                //f.Controls.Add(es);
-            }
+                buttonsClicks(3);
         }
     }
 }
